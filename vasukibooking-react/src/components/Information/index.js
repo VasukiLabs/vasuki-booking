5c8c84f5f609ec9a3f5de50fade2013e8b3c377e
Hritik Bakshi <hritikbakshi@Hritiks-MacBook-Pro.local>
2023-04-01 21:04:21 +0530

    UI changes

--- vasukibooking-react/src/components/Information/index.js
+++ vasukibooking-react/src/components/Information/index.js
@@ -1,14 +1,47 @@
-import { useReducer } from "react";
+import { FormControl, FormErrorMessage, Text } from "@chakra-ui/react";
+import { useReducer, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateUserInfo } from "../../features/booking/bookingSlice";
 
 const YourInformation = ({ setSelectedMenu }) => {
     const dispatch = useDispatch();
     const { firstName, lastName, phone, email, gender, notes } = useSelector(state => state.booking);
+    const [errors, setErrors] = useState({})
 
     const handleValidation = () => {
+        setErrors({})
+        // valid email regex rule.
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        setSelectedMenu(3)
+        if (!firstName || !lastName || !email || !phone) {
+            if (!firstName) {
+                setErrors((prev) => {
+                    return { ...prev, firstName: 'Please enter first name' }
+                })
+            }
+            if (!lastName) {
+                setErrors((prev) => {
+                    return { ...prev, lastName: 'Please enter last name' }
+                })
+            }
+            if (!email) {
+                setErrors((prev) => {
+                    return { ...prev, email: 'Please enter email' }
+                })
+            }
+            if (!re.test(email)) {
+                setErrors((prev) => {
+                    return { ...prev, email: 'Please enter valid email' }
+                })
+            }
+            if (!phone) {
+                setErrors((prev) => {
+                    return { ...prev, phone: 'Please enter phone number' }
+                })
+            }
+        } else {
+            setSelectedMenu(3)
+        }
     }
 
     return (
@@ -17,43 +50,49 @@ const YourInformation = ({ setSelectedMenu }) => {
                 <i className="fa-solid fa-left-long back" style={{ "color": "#ffffff" }} onClick={() => setSelectedMenu(1)}></i>
                 <span>Your Information</span>
             </div>
-            <div className="vasuki-section-form vasuki-customer-info">
-                <div className="vasuki-form-field">
-                    <label for="first-name" className="required">First Name</label>
-                    <input type="text" id="first-name" value={firstName} onChange={(e) => dispatch(updateUserInfo({ type: 'firstName', data: e.target.value }))} name="first-name" placeholder="First Name" required />
-                </div>
-                <div className="vasuki-form-field">
-                    <label for="last-name" className="required">Last Name</label>
-                    <input type="text" id="last-name" name="last-name" value={lastName} onChange={(e) => dispatch(updateUserInfo({ type: 'lastName', data: e.target.value }))} placeholder="Last Name" required />
-                </div>
-                <div className="vasuki-form-field">
-                    <label for="email" className="required">Email</label>
-                    <input type="email" id="email" name="email" value={email} onChange={(e) => dispatch(updateUserInfo({ type: 'email', data: e.target.value }))} placeholder="Email" required />
-                </div>
-                <div className="vasuki-form-field">
-                    <label for="phone" className="required">Phone</label>
-                    <input type="tel" id="phone" value={phone} onChange={(e) => dispatch(updateUserInfo({ type: 'phone', data: e.target.value }))} name="phone" placeholder="Phone" required />
-                </div>
-                <div className="vasuki-form-field">
-                    <p>Gender</p>
-                    <div className="radio-field">
-                        <input type="radio" id="male" name="gender" value="male" />
-                        <label for="male">Male</label>
+            <form>
+                <FormControl className="vasuki-section-form vasuki-customer-info" isInvalid>
+                    <div className="vasuki-form-field">
+                        <label for="first-name" className="required">First Name</label>
+                        <input type="text" id="first-name" value={firstName} onChange={(e) => dispatch(updateUserInfo({ type: 'firstName', data: e.target.value }))} name="first-name" placeholder="First Name" required />
+                        <FormErrorMessage fontSize={'10px'}>{errors['firstName'] && errors['firstName']}</FormErrorMessage>
                     </div>
-                    <div className="radio-field">
-                        <input type="radio" id="female" name="gender" value="female" />
-                        <label for="female">Female</label>
+                    <div className="vasuki-form-field">
+                        <label for="last-name" className="required">Last Name</label>
+                        <input type="text" id="last-name" name="last-name" value={lastName} onChange={(e) => dispatch(updateUserInfo({ type: 'lastName', data: e.target.value }))} placeholder="Last Name" required />
+                        <FormErrorMessage fontSize={'10px'}>{errors['lastName'] && errors['lastName']}</FormErrorMessage>
                     </div>
-                    <div className="radio-field">
-                        <input type="radio" id="other" name="gender" value="other" />
-                        <label for="other">Other</label>
+                    <div className="vasuki-form-field">
+                        <label for="email" className="required">Email</label>
+                        <input type="email" id="email" name="email" value={email} onChange={(e) => dispatch(updateUserInfo({ type: 'email', data: e.target.value }))} placeholder="Email" required />
+                        <FormErrorMessage fontSize={'10px'}>{errors['email'] && errors['email']}</FormErrorMessage>
                     </div>
-                </div>
-                <div className="vasuki-form-field">
-                    <label for="notes">Additional Notes</label>
-                    <textarea type="notes" id="notes" name="notes" value={notes} onChange={(e) => dispatch(updateUserInfo({ type: 'notes', data: e.target.value }))} placeholder="Type extra notes here"></textarea>
-                </div>
-            </div>
+                    <div className="vasuki-form-field">
+                        <label for="phone" className="required">Phone</label>
+                        <input type="tel" id="phone" value={phone} onChange={(e) => dispatch(updateUserInfo({ type: 'phone', data: e.target.value }))} name="phone" placeholder="Phone" required />
+                        <FormErrorMessage fontSize={'10px'}>{errors['phone'] && errors['phone']}</FormErrorMessage>
+                    </div>
+                    <div className="vasuki-form-field">
+                        <p>Gender</p>
+                        <div className="radio-field">
+                            <input type="radio" id="male" name="gender" value="male" />
+                            <label for="male">Male</label>
+                        </div>
+                        <div className="radio-field">
+                            <input type="radio" id="female" name="gender" value="female" />
+                            <label for="female">Female</label>
+                        </div>
+                        <div className="radio-field">
+                            <input type="radio" id="other" name="gender" value="other" />
+                            <label for="other">Other</label>
+                        </div>
+                    </div>
+                    <div className="vasuki-form-field">
+                        <label for="notes">Additional Notes</label>
+                        <textarea type="notes" id="notes" name="notes" value={notes} onChange={(e) => dispatch(updateUserInfo({ type: 'notes', data: e.target.value }))} placeholder="Type extra notes here"></textarea>
+                    </div>
+                </FormControl>
+            </form>
             <div className="vasuki-next" onClick={handleValidation}>
                 <button className="vasuki-next-button">
                     Continue
--- vasukibooking-react/src/components/SlotBooking/index.js
+++ vasukibooking-react/src/components/SlotBooking/index.js
@@ -1,14 +1,16 @@
 import MiniCalendar from "../Calendar/MiniCalendar";
 import { add, format } from 'date-fns'
 import { useEffect, useRef, useState } from "react";
-import { Box, Center, SimpleGrid } from "@chakra-ui/react";
+import { Box, Center, Flex, SimpleGrid } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateDateTime } from "../../features/booking/bookingSlice";
 
 const SlotBooking = ({ setSelectedMenu }) => {
     const { date, time } = useSelector(state => state.booking);
+    const interval = 30;
     const dispatch = useDispatch();
     const [timesArray, setTimesArray] = useState(null);
+    const [dateTime, setDateTime] = useState(false)
 
     useEffect(() => {
         if (!date) return
@@ -18,18 +20,36 @@ const SlotBooking = ({ setSelectedMenu }) => {
 
     }, [date])
 
+    const handleTimeSelection = (t) => {
+        setTimesArray((prev) => {
+            const newTimes = prev.map(time => {
+                if(t.time === time.time) {
+                    return {...time, selected: true}
+                }
+                return {...time, selected: false}
+            })
+
+            return newTimes;
+        })
+        setDateTime(`${format(date, 'PPP')} ${t.time}`)
+        dispatch(updateDateTime({ type: 'time', data: t }))
+    }
+
 
     const getTimes = () => {
         if (!date) return
 
         const beginning = add(date, { hours: 9 })
         const end = add(date, { hours: 16 })
-        const interval = 15
 
         const times = []
 
         for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
-            times.push(format(i, 'kk:mm'))
+            if(i === beginning){
+                times.push({ selected: true, time: format(i, 'kk:mm a')})
+                setDateTime(`${format(date, 'PPP')} ${format(i, 'kk:mm a')}`)
+            }
+            else times.push({ selected: false, time: format(i, 'kk:mm a')})
         }
 
         return times
@@ -43,10 +63,11 @@ const SlotBooking = ({ setSelectedMenu }) => {
             </div>
             <div style={{overflowY: 'auto'}}>
                 <MiniCalendar minW='100%' selectRange={false} />
-
+                <Flex paddingX={'20px'} fontSize={'15px'} fontWeight={'bold'}>Meeting Duration: {interval} Minutes</Flex>
+                {dateTime && <Flex paddingX={'20px'} fontSize={'15px'} fontWeight={'bold'}>{dateTime}</Flex>}
                 {date && <SimpleGrid minChildWidth='100px' spacing='20px' padding='20px'>
                     {timesArray && timesArray.length > 0 && timesArray.map((t, i) => {
-                        return <Center key={`time-${i}`} bg='gray' _hover={{ bg: 'darkgray' }} cursor={'pointer'} rounded={'3px'} justifyContent={'center'} height='30px' width='80px' onClick={() => dispatch(updateDateTime({ type: 'time', data: t }))}>{t}</Center>
+                        return <Center key={`time-${i}`} bg={t.selected ? 'black' : 'darkgray'} color={t.selected && 'white'} cursor={'pointer'} rounded={'3px'} justifyContent={'center'} height='30px' width='80px' onClick={() => handleTimeSelection(t)}>{t.time}</Center>
                     })}
                 </SimpleGrid>}
 
@@ -54,7 +75,7 @@ const SlotBooking = ({ setSelectedMenu }) => {
 
             </div>
             <div className="vasuki-next" onClick={() => setSelectedMenu(2)}>
-                <button className="vasuki-next-button">
+                <button className="vasuki-next-button" disabled={!date && true}>
                     Continue
                 </button>
             </div>
